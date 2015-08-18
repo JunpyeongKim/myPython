7b855c3b20f029632a55e98a8330dfca38ca2c4b
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-18 15:17:00 +0900

    [byteofpython] 14.2. Classes 14.3. Methods

---
+++ byteofpython/oop/simplestclass.py
@@ -0,0 +1,12 @@
+__author__ = 'Junpyeong Kim'
+
+
+# simplestclass.py
+# 14.2. Classes
+# - http://www.swaroopch.com/notes/python/#class
+
+class Person:
+    pass # An empty block
+
+p = Person()
+print(p)

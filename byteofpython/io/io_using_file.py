ae738c9715a594b09e39188eac3fdafc3c218953
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-20 15:43:46 +0900

    [byteofpython] 15.2. Files - comments

--- byteofpython/io/io_using_file.py
+++ byteofpython/io/io_using_file.py
@@ -4,6 +4,8 @@ __author__ = 'Junpyeong Kim'
 # io_using_file.py
 # 15.2. Files
 # - http://www.swaroopch.com/notes/python/#_files
+#   - by creating an object of the file class
+#   - The ability depends on the mode for the file opening
 
 
 poem = '''\
@@ -14,9 +16,17 @@ if you wanna make your work also fun:
 '''
 
 # Open for 'w'riting
+# - the mode
+#   - read mode('r'), write mod('w'), append mode('a'),
+#   - text mode('t'), binary mode('b')
+# - $ help(open)
+# - By default, 'rt' mode, i.e. 'read text file' is the default mode.
+#   - open() considers the file to be a 't'ext file and opens it in 'r'ead mode.
 f = open('poem.txt', 'w')
+
 # Write text to file
 f.write(poem)
+
 # Close the file
 f.close()
 

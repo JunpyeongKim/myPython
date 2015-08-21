040a41a3845d98a841af505493925a6cb1df5a65
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-21 15:21:02 +0900

    [byteofpython] 16.1. Errors & 16.2. Exceptions

---
+++ byteofpython/exceptions/exceptions_errors.py
@@ -0,0 +1,12 @@
+__author__ = 'Junpyeong Kim'
+
+
+# exceptions_errors.py
+# 16.1 Errors
+# - http://www.swaroopch.com/notes/python/#_errors
+
+Print "Hello World"
+# Python raises a syntax error, i.e. SyntaxError
+# - What an error handler for this error does.
+#   - SyntaxError and also the location.
+# ==> print "Hello World"

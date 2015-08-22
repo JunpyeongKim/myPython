de3ad3395647336c4d684c4dbd7a3856a42a22e8
Junpyeong Kim <junpyeong.kim@sk.com>
2015-08-23 01:29:47 +0900

    [byteofpython] 11.3. A module's name

--- byteofpython/module/mymodule_demo2.py
+++ byteofpython/module/mymodule_demo2.py
@@ -3,10 +3,12 @@
 # - http://www.swaroopch.com/notes/python/#_making_your_own_modules
 
 # always recommended to prefer the import statement.
+# - In general, you should avoid using the from ... import statement.
 # - should be placed in the same directory or in sys.path
 from mymodule import say_hi, __version__
 
 # should avoid using import-star
+# - In general, you should avoid using the from ... import statement.
 # - should be placed in the same directory or in sys.path
 # from mymodule import *
 

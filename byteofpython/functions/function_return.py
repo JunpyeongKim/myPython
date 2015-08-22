1503a3e3003cf92241ba65170066f393c844495e
Junpyeong Kim <junpyeong.kim@sk.com>
2015-08-22 21:15:39 +0900

    [byteofpython] 10.7. The return statement

---
+++ byteofpython/functions/function_return.py
@@ -0,0 +1,22 @@
+# function_return.py
+# 10.7. The return statement
+# - http://www.swaroopch.com/notes/python/#the_return_statement
+
+
+def maximum(x, y):
+    if x > y:
+        return x
+    elif x == y:
+        return 'The numbers are equal'
+    else:
+        return y
+
+print maximum(2, 3)
+
+
+# None : a special type in Python. --> nothingness
+# Every function implicitly contains a return None statement at the end.
+def some_function():
+    pass
+
+print some_function()

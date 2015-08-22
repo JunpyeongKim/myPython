8491dcdffd80a9ae6e880faa6e8e901408f66855
Junpyeong Kim <junpyeong.kim@sk.com>
2015-08-22 16:10:21 +0900

    [byteofpython] 9.3. The for loop

---
+++ byteofpython/controlflow/for.py
@@ -0,0 +1,14 @@
+# for.py
+# 9.3. The for loop
+# - http://www.swaroopch.com/notes/python/#_the_for_loop
+
+# range() generates a sequence of numbers.
+# - only one number at a time.
+# - the step count, i.e. 2 : range(1, 5, 2)
+# - e.g. list(range(1, 5))
+for i in range(1, 5):
+    print i
+# optional
+# - is always executed once after the for loop is over unless a break statement is encountered.
+else:
+    print 'The for loop is over'

985ef7062b77b3516eaeea227cd60b1fb99c1744
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-26 14:38:54 +0900

    [byteofpython] 12.8. More About Strings

---
+++ byteofpython/datastructures/ds_str_methods.py
@@ -0,0 +1,19 @@
+# ds_str_methods.py
+# 12.8. More About Strings
+# - http://www.swaroopch.com/notes/python/#more_strings
+
+# This is a string object
+name = 'Swaroop'
+
+if name.startswith('Swa'):
+    print 'Yes, the string starts with "Swa"'
+
+if 'a' in name:
+    print 'Yes, it contains the string "a"'
+
+if name.find('war') != -1:
+    print 'Yes, it contains the string "war"'
+
+delimiter = '_*_'
+mylist = ['Brazil', 'Russia', 'India', 'China']
+print delimiter.join(mylist)

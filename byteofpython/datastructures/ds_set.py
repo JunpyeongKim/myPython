73892e7eb29938ac3c60d07b69cbcabce615563a
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-26 13:52:34 +0900

    [byteofpython] 12.6. Set

---
+++ byteofpython/datastructures/ds_set.py
@@ -0,0 +1,37 @@
+# ds_set.py
+#  12.6. Set
+# - http://www.swaroopch.com/notes/python/#set
+
+bri = set(['brazil', 'russia', 'india'])
+print 'bric is', bri
+
+if 'india' in bri:
+    print 'india is in bri'
+else:
+    print 'india is not in bri'
+
+if 'usa' in bri:
+    print 'usa is in bri'
+else:
+    print 'usa is not in bri'
+
+bric = bri.copy()
+print '(Copy) birc is', bric
+
+bric.add('china')
+print '(Add) bric is', bric
+
+if bric.issuperset(bri):
+    print 'bric is superset of bri'
+else:
+    print 'bric is not superset of bri'
+
+bri.remove('russia')
+print '(Remove) bri is', bri
+
+if bri & bric:  # OR bri.intersection(bric)
+    print 'intersected set is', bri & bric
+# if bri.intersection(bric):
+#     print 'intersected set is', bri.intersection(bric)
+else:
+    print 'not intersected'

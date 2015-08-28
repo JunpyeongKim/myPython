d869f5960a80c073c6a502b96ce39bfed51000f4
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-28 14:10:41 +0900

    [byteofpython] 12.3. Tuple

--- byteofpython/datastructures/ds_using_tuple.py
+++ byteofpython/datastructures/ds_using_tuple.py
@@ -1,14 +1,20 @@
 # ds_using_tuple.py
 # 12.3. Tuple
 # - http://www.swaroopch.com/notes/python/#tuple
+#   - immutable
+#   - an empty tuple : ()
+#   - a tuple with a single item : (2, )
 
 # I would recommend always using parentheses
 # to indicate start and end of tuple
 # event though parentheses are optional.
 # Explicit is better than implicit.
 zoo = ('python', 'elephant', 'penguin')
+# len() indicates that a tuple is a sequence as well.
 print 'Number of animals in the zoo is', len(zoo)
 
+# does not lose its identity
+# a list within a list, a tuple within a list, a tuple within a tuple, a list within a tuple
 new_zoo = 'monkey', 'camel', zoo
 print 'Number of cages in the new zoo is ', len(new_zoo)
 print 'All animals in new zoo are', new_zoo

6236f1b7dd409a2119e680a0a9fc61856fb55cdc
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-28 14:02:21 +0900

    [byteofpython] 12.1. List & 12.2. Quick Introduction To Object And Classes - comments

--- byteofpython/datastructures/ds_using_list.py
+++ byteofpython/datastructures/ds_using_list.py
@@ -1,8 +1,15 @@
 # ds_using_list.py
 # 12.1. List & 12.2. Quick Introduction To Object And Classes
+# - http://www.swaroopch.com/notes/python/#list
+#   - Data structures are used to store a collection of related data.
 # - http://www.swaroopch.com/notes/python/#_quick_introduction_to_objects_and_classes
+#   - List
+#       - holds an ordered collection of items.
+#       - is a mutable data type.
+#   - help(list)
 
 # This is my shopping list
+# - can add any kind of object to a list.
 shoplist = ['apple', 'mango', 'carrot', 'banana']
 
 print 'I have', len(shoplist), 'items to purchase'
@@ -16,6 +23,7 @@ shoplist.append('rice')
 print 'My shopping list is now', shoplist
 
 print 'I will sort my list now'
+# affects the list itself.
 shoplist.sort()
 print 'Sorted shopping list is', shoplist
 

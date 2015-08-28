b85352da7478a2fbff1919ba10e61c66677ecb49
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-28 14:37:04 +0900

    [byteofpython] 12.5. Sequence - comments

--- byteofpython/datastructures/ds_seq.py
+++ byteofpython/datastructures/ds_seq.py
@@ -1,6 +1,13 @@
 # ds_seq.py
 # 12.5. Sequence
 # - http://www.swaroopch.com/notes/python/#sequence
+#   - lists, tuples, and strings
+#   - major features
+#       1. membership tests (i.e. in / not in)
+#       2. indexing operators
+#       3. slicing
+#   - The great thing about sequences is that
+#       - you can access tuples, lists, strings all in the same way.
 
 shoplist = ['apple', 'mango', 'carrot', 'banana']
 name = 'swaroop'
@@ -14,6 +21,9 @@ print 'Item -1 is', shoplist[-1]
 print 'Item -2 is', shoplist[-2]
 print 'Character 0 is', name[0]
 
+# The slicing operation by a colon --> [start:end:step]
+#   - start, end, step : optional
+#   - by default, the step size is 1
 # Slicing on a list #
 print 'Item 1 to 3 is', shoplist[1:3]
 print 'Item 2 to end is ', shoplist[2:]

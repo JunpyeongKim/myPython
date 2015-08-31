7eba33a527049018567d3d53a37e924a06e764a5
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-31 16:01:27 +0900

    [game] 5. Jokes

---
+++ game/jokes.py
@@ -0,0 +1,25 @@
+# jokes.py
+# 5. Jokes
+# - Invent Your Own Computer Games with Python 3e, IAl Sweigart
+#   - http://inventwithpython.com/inventwithpython_3rd.pdf
+#       - http://inventwithpython.com/chapter5.html
+#       - http://inventwithpython.com/jokes.py
+
+print('What do you get when you cross a snowman with a vampire?')
+raw_input()  # v2.x --> input() for v3.x
+print('Frostbite!')
+raw_input()  # v2.x --> input() for v3.x
+print('What do dentists call a astronaut\'s cavity')
+raw_input()  # v2.x --> input() for v3.x
+print('A black hole!')
+raw_input()  # v2.x --> input() for v3.x
+print('Knock knock.')
+raw_input()  # v2.x --> input() for v3.x
+print("Who's there?")
+raw_input()  # v2.x --> input() for v3.x
+print('Interrupting cow.')
+raw_input()  # v2.x --> input() for v3.x
+# end='' is keyword argument
+# print('Interrupting cow wh', end='')  # v3.x
+print 'Interrupting cow wh',  # v2.x
+print('-MOO!')

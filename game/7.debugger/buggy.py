88fe834da9ca8787c7470fe336943d6571a39717
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-08-31 16:48:00 +0900

    [game] 7.7 Find the Debugger - comments

--- game/7.debugger/buggy.py
+++ game/7.debugger/buggy.py
@@ -1,7 +1,9 @@
 # buggy.py
 # 7.7 Find the Debugger
 # - Invent Your Own Computer Games with Python 3e, IAl Sweigart
-# - http://inventwithpython.com/buggy.py
+#   - http://inventwithpython.com/chapter7.html
+#       - Syntax error, Runtime error (=Crash), Semantic error
+#   - http://inventwithpython.com/buggy.py
 
 import random
 
@@ -12,16 +14,16 @@ number2 = random.randint(1, 10)
 print('What is ' + str(number1) + ' + ' + str(number2) + '?')
 
 # semantic error
-answer = raw_input('> ')
+answer = raw_input('> ')  # v2.x --> input('> ') for v3.x
 if answer == number1 + number2:
-# if int(answer) == number1 + number2:
+# if int(answer) == number1 + number2:  # no semantic error
     print('Correct!')
 else:
     print('Nope! The answer is ' + str(number1 + number2))
 
 
 # No semantic error
-# answer = input('> ')
+# answer = input('> ')  # v2.x
 # if answer == number1 + number2:
 #     print('Correct')
 # else:

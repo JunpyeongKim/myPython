3136fa277fbf7723f01a5c840838dee2a22f980c
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-04 11:06:35 +0900

    [game] 14. Caesar Cipher - brute force mode

--- game/14.ceasarcipher/cipher.py
+++ game/14.ceasarcipher/cipher.py
@@ -12,13 +12,13 @@ MAX_KEY_SIZE = 26
 
 def getMode():
     while True:
-        print('Do you wish to encrypt or decrypt a message?')
+        print('Do you wish to encrypt or decrypt or brute force a message?')
         # mode = input().lower()  # v3.x
         mode = raw_input().lower()  # v2.x
-        if mode in 'encrypt e decrypt d'.split():
+        if mode in 'encrypt e decrypt d brute b'.split():
             return mode
         else:
-            print('Enter either "encrypt" or "e" or "decrypt" or "d".')
+            print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
 
 
 def getMessage():
@@ -37,8 +37,9 @@ def getKey():
         # if 1 <= key <= MAX_KEY_SIZE:
             return key
 
+
 def getTranslatedMessage(mode, message, key):
-    if mode[0] == 'd':
+    if mode[0] == 'd' or mode[0] == 'b':
         key = -key
 
     translated = ''
@@ -68,7 +69,16 @@ def getTranslatedMessage(mode, message, key):
 
 mode = getMode()
 message = getMessage()
-key = getKey()
+if mode[0] != 'b':
+    key = getKey()
 
+# in brute force mode
+# - Doubts may not be pleasant, but certainty is absurd.
+# - if key is 8, Lwcjba uig vwb jm xtmiaivb, jcb kmzbiqvbg qa ijaczl.
 print('Your translated text is:')
-print(getTranslatedMessage(mode, message, key))
+if mode[0] != 'b':
+    print(getTranslatedMessage(mode, message, key))
+else:
+    for key in range(1, MAX_KEY_SIZE + 1):
+        # print(key, getTranslatedMessage(mode, message, key))  # v3.x
+        print key, getTranslatedMessage(mode, message, key)  # v2.x

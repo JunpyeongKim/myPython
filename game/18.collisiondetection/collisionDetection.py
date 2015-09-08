e77248d753b9fa6963a183829584275a3af998da
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-08 19:32:14 +0900

    [game] 18. Collision Detection and Keyboard/Mouse Input - Initial code

---
+++ game/18.collisiondetection/collisionDetection.py
@@ -0,0 +1,15 @@
+# encoding=utf-8
+
+# collisionDetection.py
+
+# 18. Collision Detection and Keyboard/Mouse Input
+# - Invent Your Own Computer Games with Python 3e, IAl Sweigart
+#   - http://inventwithpython.com/chapter18.html
+#   - http://inventwithpython.com/collisionDetection.py
+#   - http://inventwithpython.com/pygameInput.py
+
+# Topics Covered In This Chapter:
+# ·        Collision Detection
+# ·        Don’t Modify a List While Iterating Over It
+# ·        Keyboard Input in Pygame
+# ·        Mouse Input in Pygame

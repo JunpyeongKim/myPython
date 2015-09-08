6ea6ab789fb102db3ed1ecdfa4ec4137c2be012a
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-08 19:31:47 +0900

    [game] 17. Graphics and Animation

---
+++ game/17.graphicanimation/animation.py
@@ -0,0 +1,21 @@
+# encoding=utf-8
+
+# animation.py
+
+# 17. Graphics and Animation
+# - Invent Your Own Computer Games with Python 3e, IAl Sweigart
+#   - http://inventwithpython.com/chapter17.html
+#   - http://inventwithpython.com/pygameHelloWorld.py
+#   - http://inventwithpython.com/animation.py
+
+# Topics Covered In This Chapter:
+# ·        Installing Pygame
+# ·        Colors and Fonts in Pygame
+# ·        Aliased and Anti-Aliased Graphics
+# ·        Attributes
+# ·        The pygame.font.Font, pygame.Surface, pygame.Rect, and pygame.PixelArray Data Types
+# ·        Constructor Functions
+# ·        Pygame’s Drawing Functions
+# ·        The blit() Method for Surface Objects
+# ·        Events
+# ·        Animation
--- game/17.graphicanimation/pygameHelloWorld.py
+++ game/17.graphicanimation/pygameHelloWorld.py
@@ -6,6 +6,7 @@
 # - Invent Your Own Computer Games with Python 3e, IAl Sweigart
 #   - http://inventwithpython.com/chapter17.html
 #   - http://inventwithpython.com/pygameHelloWorld.py
+#   - http://inventwithpython.com/animation.py
 
 # Topics Covered In This Chapter:
 # ·        Installing Pygame

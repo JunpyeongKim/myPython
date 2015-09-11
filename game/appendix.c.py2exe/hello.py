a4fbdf96303e9dc59be0b9896e58cd930de68eb9
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-11 11:30:18 +0900

    [game] Appendix C. Py2exe

---
+++ game/appendix.c.py2exe/hello.py
@@ -0,0 +1,11 @@
+# encoding=utf-8
+
+# hello.py
+
+# Appendix C. Py2exe
+# - Invent Your Own Computer Games with Python 3e, IAl Sweigart
+#   - http://inventwithpython.com/
+
+print 'Hello, World'
+
+raw_input('Enter any text or blank:')

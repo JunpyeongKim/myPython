5d3b31e7caf12c7fd2f47e48039b8536f87a0947
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-16 14:23:10 +0900

    [corepython] 1.3.5 Looking for a Pattern within a String with search() (Searching versus Matching)

---
+++ corepython/01.regularexpressions/re.search.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+# re.search.py
+
+# Core Python Application Programming 3E, Wesley J. Chun
+# - 1.3.5 Looking for a Pattern within a String with search() (Searching versus Matching)
+#
+# search(pattern, string, flags=0)
+# - search for the first occurrence of the pattern anywhere within string
+# - match object on success, None on failure
+#
+# group(num=0)
+# - show the successful match, i.e. entire match (or specific subgroup num)
+
+
+import re
+
+m = re.match('foo', 'seafood')
+if m is not None:
+    print 'match().group():', m.group()
+
+
+m = re.search('foo', 'seafood')
+if m is not None:
+    print 'search().group():', m.group()

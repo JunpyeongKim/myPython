98f3cdad1f4edff31b25b30f00cde7abcdb7031a
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-16 15:11:21 +0900

    [corepython] 1.3.4 Matching Strings with match() - change the format of result

--- corepython/01.regularexpressions/reMatch.py
+++ corepython/01.regularexpressions/reMatch.py
@@ -16,50 +16,52 @@
 
 import re
 
-#
+
 m = re.match('foo', 'foo')  # pattern matches string
-print 'm:', m
-if m is not None:   # show match if successful
-    print '...group() 1:', m.group()
+print "'foo'.match():", m
+if m is not None:  # show match if successful
+    print '...group():', m.group()
 
 
-#
 m = re.match('foo', 'bar')  # pattern does not match string
-print 'm:', m
+print "'bar'.match():", m
 if m is not None:
-    print '...group() 2:', m.group()
+    print '...group():', m.group()
 
 
-#
+print ''
 m = re.match('foo', 'foo on the table')  # match succeeds
-print 'm:', m
-print '...group() 3:', m.group()
+print "'foo on the table'.match():", m
+if m is not None:
+    print '...group():', m.group()
 
 
-#
-print 'group() 1:', re.match('foo', 'foo on  the table').group()
+print "'foo on  the table'.match().group():", re.match('foo', 'foo on  the table').group()
 
 
-#
-print 'group() 2:', re.match('foo', 'foobar on  the table').group()
+print "'foobar on  the table'.match().group():", re.match('foo', 'foobar on  the table').group()
 
 
-#
-print 'group() 3:', re.match('foo', 'foo foobar on  the table').group()
+print "'foo foobar on  the table'.match().group():", re.match('foo', 'foo foobar on  the table').group()
+
 
 # AttributeError: 'NoneType' object has no attribute 'group'
+print ''
 try:
-    print 'group() 4:', re.match('foo', 'bar is not in the middle of foo on the table').group()
+    print "'bar is not in the middle of foo on the table'.match().group():", \
+        re.match('foo', 'bar is not in the middle of foo on the table').group()
 except AttributeError, e:
-    print 'AttributeError:', e
+    print '\n\tAttributeError:', e
 
 
 try:
-    print 'group() 5:', re.match('foo', 'table under the foo').group()
+    print "'table under the foo:", \
+        re.match('foo', 'table under the foo').group()
 except AttributeError, e:
-    print 'AttributeError:', e
+    print '\n\tAttributeError:', e
 
 try:
-    print 'group() 6:', re.match('foo', 'bar on the table').group()
+    print "'bar on the table:", \
+        re.match('foo', 'bar on the table').group()
 except AttributeError, e:
-    print 'AttributeError:', e
+    print '\n\tAttributeError:', e

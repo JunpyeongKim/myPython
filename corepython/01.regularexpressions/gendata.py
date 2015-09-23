d18caf43d5beb6c9e64cf25ae6cdc22f2b166622
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-23 16:23:19 +0900

    [corepython] 1.5 A Longer Regex Examples - Example 1-5. Data Generator for Regex Exercises

---
+++ corepython/01.regularexpressions/gendata.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+# gendata.py
+
+# Core Python Application Programming 3E, Wesley J. Chun
+# - 1.5 A Longer Regex Examples
+#   - Example 1-5. Data Generator for Regex Exercises
+#       - http://cpp.wesc.webfactional.com/cpp3ev2/book3v2/ch01/gendata.py
+

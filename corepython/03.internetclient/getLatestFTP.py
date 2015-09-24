4fcbb913c27e32b3817fff84c942d1a334f1da7f
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-09-24 17:39:42 +0900

    [corepython] 3.2.6 A Client Program FTP Example

--- corepython/03.internetclient/getLatestFTP.py
+++ corepython/03.internetclient/getLatestFTP.py
@@ -7,14 +7,36 @@
 # - 3.2.6 A Client Program FTP Example
 #   - Example 3-1. FTP Download Example
 #       - http://cpp.wesc.webfactional.com/cpp3ev2/book3v2/ch03/getLatestFTP.py
+#   - https://docs.python.org/2/library/ftplib.html
+
 
 import ftplib
 import os
 import socket
 
-HOST = 'http://ftp.mozilla.org'
-DIRN = 'pub/mozilla.org/webtools'
-FILE = 'bugzilla-LATEST.tar.gz'
+'''
+    from ftplib import FTP
+    f = FTP('some.ftp.server')
+    f.login('anonymous', 'your@email.address')
+    .
+    .
+    f.quit()
+'''
+
+'''
+    from ftplib import FTP
+    f = FTP('ftp.debian.org')                # connect to host, default port
+    f.login()                                # user anonymous, passwd anonymous@
+    f.dir()
+    f.cwd('debian')                          # change into "debian" directory
+    f.dir() or f.retrlines('LIST')           # list directory contents
+    f.retrbinary('RETR README', open('README', 'wb').write)
+    f.quit()
+'''
+
+HOST = 'ftp.debian.org'     # 'ftp.mozilla.org'
+DIRN = 'debian'             #'pub/mozilla.org/webtools'
+FILE = 'README.html'        #'bugzilla-LATEST.tar.gz'
 
 
 def main():
@@ -46,6 +68,9 @@ def main():
 
     try:
         f.retrbinary('RETR %s' % FILE, open(FILE, 'wb').write)
+            # retrbinary(cmd, cb[, bs=8192[, ra]])
+            # - Download binary file
+            # - callback cb for processing each block
     except ftplib.error_perm:
         print 'ERROR: cannot read file "%s"' % FILE
         if os.path.exists(FILE):

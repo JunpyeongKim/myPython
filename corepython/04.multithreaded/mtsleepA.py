9fc0ef59e7157824be1dde1881c4b2b527eebf41
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-10-05 17:26:14 +0900

    [corepython] 4.4 The thread Module - thread

---
+++ corepython/04.multithreaded/mtsleepA.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+# mtsleepA.py
+
+# Core Python Application Programming 3E, Wesley J. Chun
+# - 4.4 The thread Module
+#   - Example 4-2. Using the thread Module
+#       - onethr.py + thread
+#       - http://cpp.wesc.webfactional.com/cpp3ev2/book3v2/ch04/mtsleepA.py
+
+
+from time import sleep, ctime
+import thread
+
+
+def loop0():
+    print 'start loop 0 at:', ctime()
+    sleep(4)
+    print 'loop 0 done at:', ctime()
+
+
+def loop1():
+    print 'start loop 1 at:', ctime()
+    sleep(2)
+    print 'loop 1 done at:', ctime()
+
+
+# start_new_thread(function, args, kwargs=None)
+def main():
+    print 'starting at:', ctime()
+    thread.start_new_thread(loop0, ())
+    thread.start_new_thread(loop1, ())
+    sleep(6)  # used as our synchronization mechanism. --> not reliable.
+    print 'all DONE at:', ctime()
+
+
+if __name__ == '__main__':
+    main()

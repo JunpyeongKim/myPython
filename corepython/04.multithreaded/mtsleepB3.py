6770c70a5dac21c3aa611ca9ca4b280ebf40dd4c
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-10-06 11:06:44 +0900

    [corepython] 4.4 The thread Module

--- corepython/04.multithreaded/mtsleepB3.py
+++ corepython/04.multithreaded/mtsleepB3.py
@@ -7,6 +7,8 @@
 # - 4.4 The thread Module
 #   - Example 4-3. Using thread and Locks
 #       - http://cpp.wesc.webfactional.com/cpp3ev2/book3v2/ch04/mtsleepB3.py
+#   - the thread module only to introduce the reader to threaded programming.
+#   - Your MT application should use higher-level modules such as the threading module,
 
 from time import sleep, ctime
 import _thread
@@ -32,9 +34,17 @@ def main():
         lock.acquire()
         locks.append(lock)
 
+    # First, we wanted to synchronize the threads, so that all the horses started out the gate around the same time
+    # and second, locks take a little bit of time to be acquired.
     for i in nloops:
         _thread.start_new_thread(loop, (i, loops[i], locks[i]))
 
+    # The final loop just sits and spins (pausing the main thread)
+    # until both locks have been released before continuing execution.
+    # Because we are checking each lock sequentially, we might be at the mercy of
+    # all the slower loops if they are more toward the beginning of the set of loops.
+    # When that lock is released, remaining locks may have already been unlocked
+    # (meaning that corresponding threads have completed execution)
     for i in nloops:
         while locks[i].locked():
             pass

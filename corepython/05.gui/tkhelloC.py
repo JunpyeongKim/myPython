57d03e1c87695d1beddaff7be214699e69541bf1
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-10-12 14:58:59 +0900

    [corepython] 5.3.3 The Label and Button Widget

--- corepython/05.gui/tkhelloC.py
+++ corepython/05.gui/tkhelloC.py
@@ -18,6 +18,11 @@ hello = Tkinter.Label(top, text='Hello World!')
 hello.pack()
 
 quit = Tkinter.Button(top, text='QUIT', command=top.quit, bg='red', fg='white')
+# The fill parameter tells it to let the QUIT button take
+# up the rest of the horizontal real estate,
+# and the expand parameter directs it to visually fill out the entire horizontal landscape,
+# stretching the button to the left and right sides of the window.
+# ==> Horizontal placement requires creating a new Frame object
 quit.pack(fill=Tkinter.X, expand=1)
 
 Tkinter.mainloop()
--- corepython/05.gui/tkhelloC3.py
+++ corepython/05.gui/tkhelloC3.py
@@ -18,6 +18,11 @@ hello = tkinter.Label(top, text='Hello World!')
 hello.pack()
 
 quit = tkinter.Button(top, text='QUIT', command=top.quit, bg='red', fg='white')
+# The fill parameter tells it to let the QUIT button take
+# up the rest of the horizontal real estate,
+# and the expand parameter directs it to visually fill out the entire horizontal landscape,
+# stretching the button to the left and right sides of the window.
+# ==> Horizontal placement requires creating a new Frame object
 quit.pack(fill=tkinter.X, expand=1)
 
 tkinter.mainloop()

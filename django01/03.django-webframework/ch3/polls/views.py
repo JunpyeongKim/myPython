6a3cac4576856f9893c41a1362b52f84a95a8844
Junpyeong Kim <junpyeong.kim@gmail.com>
2015-10-30 11:16:56 +0900

    [django01] 3.7.3 View 함수 detail() 및 폼 템플릿 작성

--- django01/03.django-webframework/ch3/polls/views.py
+++ django01/03.django-webframework/ch3/polls/views.py
@@ -21,3 +21,20 @@ def index(request):
             - 웹 프로그램 개발시 자주 사용되는 기능들을 미리 개발하여 내장 함수로 제공
     '''
     return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # django.shortcut
+    #   - def get_object_or_404(klass, *args, **kwargs):
+    #   - (*) get_list_or_404() : filter() 를 사용
+    #       - get_object_or_404() : get() 을 사용
+    question = get_object_or_404(Question, pk=question_id)
+
+    '''
+        Relation --> Question : Choice = 1 : N
+            - xxx_set 속성을 default로 제공한다
+            - ex) question.choice_set.all()
+                - Question 테이블의 question 레코드에 연결된 Choice 테이블의 레코드를 모두를 의미
+                - question.choice_set.all : template syntax에서는 ()를 사용하지 않는다
+    '''
+    return render(request, 'polls/detail.html', {'question': question})
